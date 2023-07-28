b501574c8d3bf56d3a478044eafc03ebd6e1c31c
unknown <nitin.kool.1808@gmail.com>
2023-07-28 16:54:57 +0530

    Home page ready

---
+++ src/sendEmail.ts
@@ -0,0 +1,29 @@
+import emailjs from '@emailjs/browser';
+
+const public_key = "9_66Gz36slYB-nUQo"
+const serviceID="service_s4q446u"
+const templateID="template_46pu88v"
+
+emailjs.init(public_key)
+
+const sendEmail = async(from_name:string, from_email:string, message:string, timestamp:string)=>{
+    try {
+        const data = {
+            from_email:from_email,
+            from_name:from_name,
+            message:message,
+            timestamp:timestamp
+        }
+        console.log(data)
+        await emailjs.send(serviceID, templateID, data);
+        console.log('Email sent successfully!');
+        return true;
+      } catch (error) {
+        console.error('Error sending email:', error);
+        return false;
+      }
+}
+
+export default sendEmail
+
+
