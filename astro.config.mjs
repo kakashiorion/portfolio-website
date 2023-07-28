8240cf4cbade1a9cd2720ad99ea38df2fbb641c8
unknown <nitin.kool.1808@gmail.com>
2023-07-28 18:12:54 +0530

    moved images to src/assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  experimental: {
+    assets: true,
+  },
   sites: "https://kakashiorio.github.io/",
   base: "/portfolio-website",
 });
